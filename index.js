6bf638896ee35e40ec354dd87f670148faee62a6
Felpzo the man <felipe.ls2004@aluno.ifsc.edu.br>
2023-08-23 20:17:00 +0000

    boa

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import cena0 from "./cena0.js"
-import config from "./config.js"
+import cena0 from './cena0.js'
+import config from './config.js'
 
 class Game extends Phaser.Game {
-    constructor(){
+    constructor() {
         super(config)
 
-        this.scene.add('cena0' , cena0) 
+        this.scene.add('cena0', cena0)
 
         this.scene.start('cena0')
     }
