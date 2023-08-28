63a8239e547bdba12f674b2dde381c9095f60ffb
Felpzo the man <felipe.ls2004@aluno.ifsc.edu.br>
2023-08-28 19:40:55 +0000

    botões

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -4,8 +4,20 @@ export default class cena0 extends Phaser.Scene {
   }
 
   preload () {
+    /* imagem de fundo */
     this.load.image('ifsc-sj-2014', '../assets/ifsc-sj-2014.png')
+
+    /* personagem */
     this.load.spritesheet('derek', '../assets/Derek.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+    /* botões */
+    this.load.spritesheet('direita', '../assets/direita.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+    this.load.spritesheet('esquerda', '../assets/esquerda.png', {
       frameWidth: 64,
       frameHeight: 64
     })
@@ -13,15 +25,19 @@ export default class cena0 extends Phaser.Scene {
 
   create () {
     this.imagem = this.add.image(400, 225, 'ifsc-sj-2014')
+
+    /* personagem */
     this.personagem = this.physics.add.sprite(400, 225, 'derek')
-      .setInteractive()
-      .on('pointerdown', () => {
-        this.personagem.anims.play('derek-direita')
-        this.personagem.setVelocityX(100)
-      })
-      .on('pointerup', () => {
-        this.personagem.setVelocityX(0)
-      })
+
+    /* animações */
+    this.anims.create({
+      key: 'derek-parado',
+      frames: this.anims.generateFrameNumbers('derek', {
+        start: 0,
+        end: 0
+      }),
+      frameRate: 1
+    })
 
     this.anims.create({
       key: 'derek-direita',
@@ -29,10 +45,35 @@ export default class cena0 extends Phaser.Scene {
         start: 8,
         end: 11
       }),
-      frameRate: 6,
+      frameRate: 8,
       repeat: -1
     })
-  }
+
+    /* botões */
+    this.direita = this.add.sprite(180, 350, 'direita', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.direita.setFrame(1)
+        this.personagem.anims.play('derek-direita', true)
+        this.personagem.setVelocityX(200)
+      })
+      .on('pointerup', () => {
+        this.direita.setFrame(0)
+        this.personagem.anims.play('derek-parado')
+        this.personagem.setVelocityX(0)
+      })
+    this.esquerda = this.add.sprite(50, 400, 'esquerda, 0')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.esquerda.setFrame(1)
+        this.personagem.anims.play('derek-esquerda', true)
+        this.personagem.setVelocityX(200)
+      })
+      .on('pointerup', () => {
+        this.direita.setFrame(0)
+        this.personagem.anims.play('derek-parado')
+        this.personagem.setVelocityX(0)
+  })
 
   update () { }
 }
